25c3fac377756d96b69899f30c743b5e47b14853
Jens Lindahl <jensctl@gmail.com>
2018-06-27 08:46:58 +0200

    Add HiDPI support Add support for MacOS native frameworks with cmake

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <QApplication>
-#include <QDebug>
-#include <QStorageInfo>
 #include <gui/mainwindow.h>
-#include <QRegularExpression>
 
 int main(int argc, char** argv) {
     Q_INIT_RESOURCE(icons);
 
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("LLab Volume Backup Tool");
